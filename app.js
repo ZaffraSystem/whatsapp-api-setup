d29171c5d9f7427af9e80fbfe5b9070a5f7d3391
Gabriel Moraes <120189165+GabrielMoors@users.noreply.github.com>
2023-01-18 14:39:14 -0300

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const fileUpload = require('express-fileupload');
 const axios = require('axios');
 const mime = require('mime-types');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 80;
 
 const app = express();
 const server = http.createServer(app);
